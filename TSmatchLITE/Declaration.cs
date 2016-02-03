7f28260ecfaa21eb3de59a3ce4ed2322a513fd0e
Александр Бобцов <AlexandrBobtsov@gmail.com>
2016-02-03 22:31:31 +0300

    что-то изменения

---
+++ TSmatchLITE/Declaration.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSmatch.Declaration
+{
+    /// <summary>
+    /// Declaration -- определения глобальных констант
+    /// </summary>
+    /// <journal> 20.12.2013
+    /// 7.1.2014    - добавлена секция констант Шаблонов
+    /// 23.1.14     - переход к DataTable
+    /// 12.12.15    - адаптация к TSmatch
+    /// 22.12.15    - секция типов Штампа Документа
+    /// 2.1.2016    - моды работы с Документом
+    /// 9.1.2016    - разделитель списков в строке ";"
+    /// 24.1.16     - константы Matching_Rules, регулярнае выражения
+    /// </journal>
+    class Declaration
+    {
+        //------------ КОНСТАНТЫ ОБЩЕГОНАЗНАЧЕНИЯ -----------------
+        public const char STR_DELIMITER = ';';  // знак - резделитель в списке в строках
+        /// <summary>
+        /// F_MATCH = "TSmatch.xlsx" - имя файла таблиц приложения TSmatch
+        /// !!временно разместил вместе с Tekla Structures\версия\Environments, но надо будет уточнить в Tekla!!
+        /// </summary>
+        public const string F_MATCH = "TSmatch.xlsx";
+        public const string DIR_MATCH = @"C:\ProgramData\Tekla Structures\21.1\Environments\common\exceldesign";
+        public const string DOC_TOC = "TOC";    // Table Of Content - Лист - таблица-содержание всех Документов в TSmatch
+
+        //--------- листы TSmatch.xlsm --------------
+        public const string CONST = "Const";
+        public const string HEADER = "Header";
+        public const string LOG = "Log";
+
+        //-----------константы таблицы Документов -----------------
+        public const int DOC_TIME = 1; // дата и время последнего изменения Документа
+        public const int DOC_NAME = 2; // имя Документа
+        public const int DOC_EOL = 3; // EOL Документа
+        public const int DOC_MYCOL = 4; // количество колонок, дабавляемых слева в Документ в loadDoc
+        public const int DOC_RESLINES = 5; // число строк пятки
+        public const int DOC_MADESTEP = 6; // последний выполненный Шаг
+        public const int DOC_DIR = 7; // каталог, где лежит Документ
+        public const int DOC_FILE = 8; // файл match, содержащий Документ
+        public const int DOC_SHEET = 9; // лист Документа
+        public const int DOC_STMPTXT = 10; // текст Штампа
+        public const int DOC_STMPTYPE = 11; // тип Штампа
+        public const int DOC_STMPROW = 12; // строка Штампа
+        public const int DOC_STMPCOL = 13; // колонка Штампа
+        public const int DOC_CREATED = 14; // дата создания Документа
+        public const int DOC_PATTERN = 16; // основной шаблон Документа. Помимо заголовков Шапки,
+                                           //..он содержит описания колонок данных
+        public const int DOC_SUMMARY_PATTERN = 17; // шаблон пятки
+        public const int DOC_LOADER = 20; // Loader Документа
+        public const int DOC_MD5 = 27; // Check Sum MD5
+
+        //-----------константы моды работы с Документом -----------------
+        public enum DOC_RW_TYPE { RW, RO, CREATEOROPEN, N1, N2, N };
+        public const int READWRITE = 0;   // Мода Документа - открыть на чтение и запись -- по умолчанию
+        public const int READONLY = 3;   // Мода Документа - только чтение
+        public const int APPEND = 11;   // Мода Документа - дописывать
+        public const int CREATEOROPEN = 21;   // Мода Документа - если не найден - создать
+
+        //----------- ТИПЫ ШТАМПОВ / ТИПЫ ДОКУМЕНТОВ ----------------------
+        public const string STAMP_TYPE_EQ = "=";  // точное соответствие
+        public const string STAMP_TYPE_INC = "I"; // Includes
+        public const string STAMP_TYPE_N = "N";   // New Doc без Штампа или No Stamp
+        public const string STAMP_TYPE_N1 = "N1"; // New Doc, создавать новый Лист в самой левой позиции, если его нет; для Summary
+        public const string STAMP_TYPE_N2 = "N2"; // New Doc, если нет, создавать в Листе2
+
+        //-----------константы таблицы Процессов -----------------
+        public const string PROCESS = "Process";
+
+        public const int STEP_TIME = 0;    // Дата и время выполнения Шага. Если тут пусто или "/" - это строка-комментарий
+        public const int PROC_NAME = 1;    // Имя Процесса. В сроках Шага тут пусто
+        public const int STEP_PREVSTEP = 2;    // Предшествующий Шаг. Шаг этой строки выполняем только
+                                               //..если предыдущий Шаг выполнен или тут пусто
+        public const int STEP_NAME = 3;    // Имя Шага
+        public const int STEP_DONE = 4;    // Шаг выполнен "1", иначе - выполняем
+        public const int STEP_PARAM = 5;    // Параметры Шага
+        public const int STEP_INPDOCS = 6;    // Входные Документы Шага
+        public const int STEP_OUTDOCS = 7;    // Выходные Документы Шага
+
+        //------------ константы журнала моделей -------------------
+        public const string MODELS = "Models";
+
+        public const int MODEL_DATE = 1;    // Дата и время записи в Журнал моделей
+        public const int MODEL_NAME = 2;    // Имя модели
+        public const int MODEL_DIR = 3;    // Каталог модели
+        public const int MODEL_MADE = 4;    // Модель обработана Шагом (Made)
+        public const int MODEL_MD5 = 5;    // Контрольная сумма - MD5
+
+        //------------ константы правил Matching_Rules -------------------
+        public const string MATCHING_RULES = "Matching_Rules";
+
+        public const int RULE_DATE = 1;    // Дата и время записи Правила в TSmatch
+        public const int RULE_NAME = 2;    // Имя Правила
+        public const int RULE_TYPE = 3;    // Тип Правила
+        public const int RULE_PARS = 4;    // Параметры Правила
+        public const int RULE_DOCS = 5;    // Документы Правила
+
+        public const string ATT_DELIM = @"(${must}|,|=| |\t|\*|x|X|х|Х)";  //делимитры в Правилах
+        public const string ATT_MUST  = @"(?<must>(&'.+')|(&"".+"")|(&«.+»)|(&“.+”)"; // &обязательно
+        public const string ATT_PARAM = @"(?<param>(\$|p|р|п|P|Р|П)\w*\d)"; //параметры в Правилах
+
+        //------------ Файл TSmatchINFO.xlsx - записывается в каталог модели ----------
+        public const string SH1_MODELINFO = "ModelINFO"; //Лист общей информации по модели
+        public const string SH2_RAW = "Raw";        //Лист необработанных данных по компонентам модели
+        public const string SH3_REPORT = "Report";     //Лист - отчет по модели
+
+        public const string TS_PATH = "#TS.ModInfo.ModePath";   //#template в TOC- вместо ссылки
+                                                                //на каталог модели
+
+        //-----------константы Шаблонов -----------------
+        public const string PTRN_HDR = "A1";     // заголовки колонок   
+        public const string PTRN_WIDTH = "A3";     // ширина колонок
+        public const int PTRN_FETCH = 6;    // Fetch запрос
+
+        public const string PTRN_COPYHDR = "CopyHdr"; // указание копировать заголовок из Шаблона
+    }
+}
---
+++ TSmatchLITE/Program.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+//-- мои модули
+using Decl = TSmatch.Declaration.Declaration;
+using Log = match.Lib.Log;
+using TS = TSmatch.Tekla.Tekla;
+using FileOp = match.FileOp.FileOp;
+//-- низкоуровневые обращения, от которых хорошо бы избавиться
+using Excel = Microsoft.Office.Interop.Excel;
+using Mtr = match.Matrix.Matr;
+
+namespace TSmatchLITE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TS.GetTeklaDir();
+            Elements = TS.Read();
+            Ignore0Length();
+            int ok = 0;
+            ok += Search("Уголок", "М:С255=Ст3Гпс,Cт3гсп; П: Уголок = L $p1x$p2; lng");
+            ok += Search("Полоса", "М:С255=Ст3Гпс,Cт3гсп ; П:Полоса = — $p1*$p2");
+            //            ok += Search("Полоса", "М:С255=Ст3; П:Полоса = — $p1"); ..РАЗОБРАТЬСЯ с $1 и $1*$2
+            ok += Search("Балка", "М:С255=Ст3Гпс=Cт3гсп; П:Балка = I Б $p1");
+            // ввести 'Ш1' или "K2"  и & - обязательные элементы в разбор регулярных выражений
+            ok += Search("Труба профильная", "М:С255=Ст3Гпс=Cт3гсп; П:Гнз p1xp2xp3");
+            okSave(TS.ModInfo.ModelPath, ok);
+            new Log("=======================\nвсего подобрали комплектующие для " + ok + " элементов");
+            Console.ReadLine();
+        }
+
+        static List<TS.AttSet> Elements = new List<TS.AttSet>();
+
+        static void Ignore0Length()
+        {
+            int cnt = Elements.Count;
+            int cnt_initial = cnt;
+            for(int i=0; i< cnt; i++)
+            {
+                if (Elements[i].lng <= 0)
+                {
+                    Elements.Remove(Elements[i]);
+                    cnt--;
+                }
+            }
+            new Log("В модели " + (cnt_initial - cnt) + " элементов нулевой длины -- игнорируем их");
+        }
+
+        static int Search(string ShtN, string rule)
+        {
+            int ok = 0;
+            List<string> Comp = GetCompList(ShtN);
+            RuleParser(rule);
+            int nstr = 0;
+            foreach (var v in Elements)
+            {
+                if (SearchInComp(ShtN, Comp, nstr, v.mat, v.prf)) ok++;
+                nstr++;
+            }
+            new Log("в каталоге " + ShtN + "\tподобрали соответствие для " + ok + " элементов");
+            return ok;
+        }
+        public static List<string> GetCompList(string SheetN)
+        {
+            const int i0 = 7;
+            const string FileName = @"black1_0.xls";
+            //--- Catalog Path for PavelMSI ---
+            //const string FileDir = @"C:\Users\Pavel_Khrapkin\Desktop\TSmatch\База Севзапметалл";
+            //--- Catalog Path for Bureau ESG office ---
+            string exceldesightPath = TS.GetTeklaDir();
+            string FileDir = exceldesightPath + @"\База Севзапметалл";
+            List<string> Comp = new List<string>();
+            Excel.Workbook Wb = FileOp.fileOpen(FileDir, FileName);
+            Excel.Worksheet Sh = Wb.Worksheets[SheetN];
+            Mtr Body = FileOp.getSheetValue(Sh);
+            for (int i = i0; i <= Body.iEOL(); i++)
+                Comp.Add(Body.Strng(i, 1));
+            return Comp;
+        }
+        public static void okSave(string FileDir, int ok)
+        {
+            const string SheetN = "Report";
+            const string FileName = "TSmatchINFO.xlsx";
+            Excel.Workbook Wb = FileOp.fileOpen(FileDir, FileName, create_ifnotexist: true);
+            FileOp.SheetReset(Wb, SheetN); 
+            Excel.Worksheet Sh = Wb.Worksheets[SheetN];
+//            Mtr Body = null;
+            int n_ex_str = 0;
+            string mat = "Материал", prf = "Профиль", lng = "Длина",
+                Comp_doc = "Компонент", Comp_n = "№ стр", Comp_str = "Cтрока компонента";
+            Mtr Body = new Mtr();
+            Body.Init(new object[] { "№", mat, prf, lng, Comp_doc, Comp_n, Comp_str });
+            foreach (var v in Elements)
+            {
+                mat = v.mat;
+                prf = v.prf;
+                lng = v.lng.ToString();
+                for (int i_ok = 0; i_ok < OKnstrComp.Count; i_ok++)
+                {
+                    if (n_ex_str == OKnstrComp[i_ok])
+                    {
+                        Comp_doc = OKdocName[i_ok];
+                        Comp_n = OKnComp[i_ok].ToString();
+                        Comp_str = OKstrComp[i_ok];
+                        break;
+                    }
+                    else { Comp_doc = ""; Comp_str = ""; Comp_n = ""; }
+                }
+                Body.AddRow(new object[] { n_ex_str, mat, prf, lng, Comp_doc, Comp_n, Comp_str });
+                n_ex_str++;
+            }
+            FileOp.setRange(Sh);
+            FileOp.saveRngValue(Body);
+        }
+        /// <remarks> 26.1.2016
+        /// RuleParser разбирает текстовую строку - правило, выделяя и возвращая в списках 
+        ///     RuleMatList - части Правила, относящиеся к Материалу компонента
+        ///     RulePrfList - части, относящиеся к Профилю
+        ///     RuleOthList - остальные разделы Правила.
+        ///     
+        ///     RuleMatMust, RulePrfMust и RuleOthMust  - обязательные элементы раздела, если есть
+        ///     RuleMatNpars, RulePrfNpars и RuleOthNpars - количество числовых параметров в разделе
+        ///     
+        /// Разделы начинаются признаком раздела, например, "Материал:" и отделяются ';'
+        /// Признак раздела распознается по первой букве 'M' и завершается ':'. Поэтому
+        ///             "Профиль:" = "П:" = "Прф:" = "п:" = "Prof:"
+        /// Заглавные и строчные буквы эквивалентны, национальные символы транслитерируются.
+        /// Разделы Правила можно менять местами и пропускать; тогда они работают "по умолчанию".
+        /// '=' означает эквивалентность. Допустимы выражения "C255=Ст3=сталь3".
+        /// ',' позволяет перечислять элементы и подразделы Правила.
+        /// ' ' пробелы, табы и знаки конца строки игнорируются, однако они могут служить признаком
+        ///     перечисления, так же, как пробелы. Таким образом, названия материалов или профилей
+        ///     можно просто перечислить - это эквивалентно '='
+        /// &'..' или &".." - обязательные части при подборе соответствия. Если части, разделенные 
+        ///     знаками '=', запятыми или пробелами - это альтернативы, возможные варианты, то при
+        ///     указании &'..' ' наличие такой части в результате обязательно.
+        ///     Обязательный элемент в разделе Правила может быть толькот один.   
+        /// Параметры   - последовательность символов вида "$p1" или просто "p1" или "Параметр235".
+        ///               Параметры начинаются со знака '$' или 'p'  и кончаются цифрой. 
+        ///               Их значения подставляются из значений и атрибутов компонента в модели.
+        ///               Номер параметра в правиле неважен- он заменяется в TSmatch на номер
+        ///               по порядку следования параметров в правиле автоматически.
+        /// Наличие двух и более признаков разделов без разделяющго знака ';' - ошибка.
+        /// </remarks>
+        static List<string> RuleMatList = new List<string>();
+        static List<string> RulePrfList = new List<string>();
+        static List<string> RuleOthList = new List<string>();
+
+        static string RuleMatMust, RulePrfMust, RuleOthMust;
+        static int RuleMatNpars = 0, RulePrfNpars = 0, RuleOthNpars = 0;
+
+        static void RuleParser(string rule)
+        {
+            Log.set("RuleParser(\"" + rule + "\")");
+            const string rM = "(m|м).*:", rP = "(п|p).*:";
+            Regex regM = new Regex(rM, RegexOptions.IgnoreCase);
+            Regex regP = new Regex(rP, RegexOptions.IgnoreCase);
+            RuleMatList.Clear(); RulePrfList.Clear(); RuleOthList.Clear();
+            RuleMatNpars = RulePrfNpars = RuleOthNpars = 0;
+            string[] tmp = rule.Split(';');
+            foreach (var s in tmp)
+            {
+                string x = s;
+                while (x.Length > 0)
+                {
+                    if (Regex.IsMatch(x, rM, RegexOptions.IgnoreCase))
+                        x = attParse(x, regM, RuleMatList, ref RuleMatNpars);
+                    if (Regex.IsMatch(x, rP, RegexOptions.IgnoreCase))
+                        x = attParse(x, regP, RulePrfList, ref RulePrfNpars);
+                    if (x != "")
+                        x = attParse(x, null, RuleOthList, ref RuleOthNpars);
+                }
+            }
+            Log.exit();
+        }
+        static string attParse(string str, Regex reg, List<string> lst, ref int n)
+        {
+            if(reg != null) str = reg.Replace(str, "");
+
+
+            string[] parametrs = Regex.Split(str, Decl.ATT_DELIM);
+            foreach (var par in parametrs)
+            {
+                if (string.IsNullOrEmpty(par)) continue;
+                bool parPars = false;
+                if (Regex.IsMatch(par, Decl.ATT_PARAM)) { n++; parPars = true; }
+                if (!string.IsNullOrWhiteSpace(par)
+                    && !Regex.IsMatch(par, Decl.ATT_DELIM)
+                    && !parPars) lst.Add(par.ToUpper());
+                str = str.Replace(par, "");
+            }
+            if (str != "") Log.FATAL("строка \"" + str + "\" разобрана не полностью");
+            return str;
+        }
+
+        static List<string> OKmat = new List<string>();
+        static List<string> OKprf = new List<string>();
+        static List<string> OKdocName = new List<string>();
+        static List<string> OKstrComp = new List<string>();
+        static List<int> OKnComp = new List<int>();
+        static List<int> OKnstrComp = new List<int>();
+        //!!-------------------------------------------------------
+        static bool SearchInComp(string docName, List<string> Comp, int nstr, string mat, string prf)
+        {
+            Log.set("SearchInComp");
+            bool found = false;
+            mat = mat.ToUpper(); prf = prf.ToUpper();
+            List<int> matPars = GetPars(mat);
+            List<int> prfPars = GetPars(prf);
+            int n = 0;
+            foreach (var s in Comp)
+            {
+                n++;
+                if (string.IsNullOrWhiteSpace(s)) continue;
+                string str = s.ToUpper();
+                if (!IContains(RuleMatList, mat)) continue;
+//                if (!str.Contains(mat)) continue;
+                if (!IContains(RulePrfList, prf)) continue;
+                List<int> CompPars = GetPars(str);
+                int i = 0;
+                foreach (int x in prfPars)
+                {
+                    if (x != CompPars[i]) break;
+                    i++;
+                }
+                if (i < prfPars.Count) continue;
+
+                int iok = 0;
+                foreach(var ok in OKnComp)
+                {
+                    if (OKnstrComp[iok] == nstr)
+                        Log.Warning("Этот компонент уже в OK"
+                            +"\nnstr=" + nstr + " mat = " + mat + "\tprf = " + prf + "\t" + OKdocName[iok]+"\t"+OKstrComp[iok]);
+                    iok++;
+                }
+
+                found = true;
+                OKmat.Add(mat);
+                OKprf.Add(prf);
+                OKdocName.Add(docName);
+                OKstrComp.Add(str);
+                OKnComp.Add(n);
+                OKnstrComp.Add(nstr);
+                break;
+            }
+            Log.exit();
+            return found;
+        }
+        /// <summary>
+        /// IContains(List<string> lst, v) возвращает true, если в списке lst есть строка, содержащаяся в v
+        /// </summary>
+        /// <param name="lst"></param>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        static bool IContains(List<string> lst, string v)   
+        {
+            bool flag = false;
+            foreach(string s in lst)
+                if (v.Contains(s)) { flag = true; break; }
+            return flag;
+        }
+//!!-------------------------------------------------------------
+        /// <summary>
+        /// GetParse(str) разбирает строку раздела компонента, выделяя параметры.
+        ///         Названия материалов, профилей и другие нечисловые подстроки игнорируются.
+        /// </summary>
+        /// <param name="str">входная строка раздела компонента</param>
+        /// <returns>List<int>возвращаемый список найденых параметров</int></returns>
+        static List<int> GetPars(string str)
+        {
+            const string VAL = @"\d+";
+            List<int> pars = new List<int>();
+            string[] pvals = Regex.Split(str, Decl.ATT_DELIM);
+            foreach (var v in pvals)
+            {
+                if (string.IsNullOrEmpty(v)) continue;
+                if (Regex.IsMatch(v, VAL))
+                    pars.Add(int.Parse(Regex.Match(v, VAL).Value));
+            }
+            return pars;
+        }
+    } // end class
+} // end namespace
